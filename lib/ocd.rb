3c0134870bc8d1e2dd45d5ff0c940cab75c6d421
Aiden Scandella <aiden.scandella@socrata.com>
2011-04-30 09:29:54 -0700

    Extracting name, season, ep number properly

--- lib/ocd.rb
+++ lib/ocd.rb
@@ -7,11 +7,27 @@ class OCD
   def scan
     Dir.glob(@config.directory + "/*.{" +
              @config.extensions.join(',') + "}") do |f|
-      puts f
+      basics = File.basename(f)
+      episode = extract_episode(basics)
+      if episode
+        puts "#{basics}: #{episode.inspect}"
+      end
+    end
+  end
+
+  def extract_episode(name)
+    if name =~ @@ep_pattern
+      match = $~.to_a
+      return nil if match.length < 4
+      ep = Hashie::Mash.new
+      ep.show_name, ep.season, ep.episode = match[1], match[2], match[3]
+      return ep
     end
   end
 
   def print_config
     puts @config.inspect
   end
+
+  @@ep_pattern = Regexp.compile(%r{(.*)\.[sS]([0-9]{1,2})[eE]([0-9]{1,2}).*})
 end
