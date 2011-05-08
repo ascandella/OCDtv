e0ed93079ec8a7a1f680821faf846d948b9d6877
Aiden Scandella <aiden.scandella@socrata.com>
2011-05-08 13:34:46 -0700

    version 0.1: first working version

--- lib/ocd.rb
+++ lib/ocd.rb
@@ -1,17 +1,24 @@
 class OCD
-
   def initialize(config)
     @config = config
+    logger.level = Logger::INFO
   end
 
   def scan
-    Dir.glob(@config.directory + "/*.{" +
-             @config.extensions.join(',') + "}") do |f|
+    Dir.glob(@config['directory'] + "/*.{" +
+             @config['extensions'].join(',') + "}") do |f|
       basics = File.basename(f)
       episode = extract_episode(basics)
+
       if episode
-        dir = find_directory(episode)
-        puts episode.show_name + ": " + dir
+        dir = find_name_with_season(
+          @config['directory'], episode[:show_name], episode[:season])
+        logger.info("Moving #{f} to #{dir.path}")
+        begin
+          FileUtils.mv(f, dir.path)
+        rescue => ex
+          logger.error("Couldn't move file: #{ex.inspect}")
+        end
       end
     end
   end
@@ -21,28 +28,34 @@ private
   def extract_episode(name)
     if name =~ @@ep_pattern
       match = $~.to_a
-      return nil if match.length < 4
-      return Hashie::Mash.new({
+      return {
         :show_name => match[1],
         :season => match[2].to_i,
         :episode => match[3].to_i
-      })
+      } if match.length > 3
     end
   end
 
-  def find_directory(episode)
-    home = @config.directory
-    guess = home + "/" + episode.show_name
-    if !Dir.exists? guess
-      guess = home + "/" + episode.show_name.gsub('.', ' ')
-      return nil if !Dir.exists? guess
+  def find_name_with_season(path, name, season)
+    [lambda {|n, s| "#{n}/Season #{s}"},
+     lambda {|n, s| "#{n}/#{n} #{s}"},
+     lambda {|n, s| "#{n} #{s}"},
+     lambda {|n, s| n}]
+     .each do |finder|
+      guess = File.join(path, finder.call(name, season))
+      break Dir.open(guess) if Dir.exists?(guess)
+      guess.gsub!('.', ' ')
+      break Dir.open(guess) if Dir.exists?(guess)
     end
-
-    return guess
   end
 
-  # I wanted to do this in the least readable way possible :)
+  # I wanted to do this in the least readable way possible.
   # Also, for some reason it wouldn't compile insensitive, so
   # I spelled out the cases. Good times.
-  @@ep_pattern = Regexp.compile(%r{(.*)\.[sS]([0-9]{1,2})[eE]([0-9]{1,2}).*})
+  @@ep_pattern = Regexp.compile(/(.*)[\. ][sS]([0-9]{1,2})[eE]([0-9]{1,2}).*/)
+
+  def logger
+    # @logger ||= Logger.new(@config[:log)
+    @logger ||= Logger.new(STDOUT)
+  end
 end
--- ocdtv.rb
+++ ocdtv.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
+
 require 'rubygems'
 require 'yaml'
 require 'hashie'
-require 'fileutils'
+require 'logger'
 
 require './lib/ocd.rb'
 
-config = File.open(
-  File.join(File.dirname(__FILE__), 'config.yml')) do |yam|
-  Hashie::Mash.new(YAML::load(yam))
-end
+config = YAML::load(File.open(File.join(
+  File.dirname(__FILE__), 'config.yml')))
 
 sweeper = OCD.new(config)
 sweeper.scan
